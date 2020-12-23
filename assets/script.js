4ad0efdf2acff47f85246c4117b441fdcd1a17b5
BtramVu <bbbvut@gmail.com>
2020-12-22 18:41:22 -0800

    addEventListener to submmit button

--- assets/script.js
+++ assets/script.js
@@ -4,33 +4,35 @@ var timeEl = document.querySelector(".time");
 var interval;
 // asign the time limit 
 var secondsLeft = 100;
-
+// run the function setTime to countdown 
 function setTime() {
-  var timerInterval = setInterval(function() {
+  timerInterval = setInterval(function() {
     secondsLeft = secondsLeft - 1 ;
     timeEl.textContent = "Time: " + secondsLeft;
 
     if(secondsLeft === 0) {
-      clearInterval(interval);
+      clearInterval(timerInterval);
       sendMessage();
     }
   }, 1000);
 }
+// send a message when time is zero 
 function sendMessage() {
     timeEl.textContent = " Time's Up! ";
     // provide place to put in high score 
 }
 
 
-// // deduct point if get the wrong answer 
+// deduct point if get the wrong answer 
 function deduct (){
   secondsLeft = secondsLeft - 10;
     timeEl.textContent = "Time: " + secondsLeft;
 }
 console.log (secondsLeft)
 
+// when the last question is ansered time stop and score record 
 function pauseTimer() {
-  clearInterval(interval);
+  clearInterval(timerInterval);
 }
 
 // HIDE AND SEEK 
@@ -42,6 +44,10 @@ var introvalue = intro.style.display;
 var score = document.querySelector("#scoreincard")
 var intpage = document.querySelector("#intpage")
 var announce = document.querySelector(".announce")
+var submitbtn=document.querySelector(".scorebutton")
+var recordpage= document.querySelector("#record-page")
+
+// hide the intro page and show the questions page 
 function show (){
   setTime();
   intro.style.display="none";
@@ -49,33 +55,36 @@ function show (){
 }
 startbtn.addEventListener("click", show)
 
+// hide the question page and show the input score page when the last question is ansered 
 function showscore(){
   questionsInCard.style.display="none";
   score.style.display= "block";
   announce.textContent= "Your score is " + secondsLeft
-  console.log ("show score")
-  
+  console.log ("show score") 
 }
-// 
-var submitbtn=document.querySelector(".scorebutton")
-var recordpage= document.querySelector("#record-page")
 
-
-function showRecord(){
- score.style.display= "none";
- recordpage.style.display="block";
- console.log (recordpage)
- console.log (score)
+// hide the score input page and show the highscore page 
+function showRecord () {
+  score.style.display = "none";
+  recordpage.style.display = "block";
+  console.log(recordpage)
+  console.log(score)
+  var intInputEl = document.querySelector("#initials")
+  var record = intInputEl.value.trim()
+  console.log("input", record)
+  var comment = document.querySelector("#comment")
+  comment.textContent = record.value
 }
 submitbtn.addEventListener("click", showRecord)
 
-
-
-
+function storeinitials() {
+  // Stringify and set "initials" key in localStorage to todos array
+  localStorage.setItem("comment", JSON.stringify(record));
+}
 
 
 // Working with questions 
-// name var for an object that has all the question value 
+// create a variable name alltheQs that is an object of all the questions
 var alltheQs = {
   question1: "A box without hinges, key, or lid, yet golden treasure inside is hid.",
   question2: "This thing all things devours.Birds, beasts, trees, flowers.Gnaws iron, bites steel; Grinds hard stones to meal;Slays king, ruins town,And beats high mountain down.",
@@ -98,7 +107,7 @@ q.innerHTML = currentQuestion
 console.log(q)
 
 
-
+// change answers for 2nd question
 var changeq2= function changeanswer2() {
   q.innerHTML= alltheQs.question2 
   btn1.textContent = "A Clock";
@@ -106,7 +115,7 @@ var changeq2= function changeanswer2() {
   btn3.textContent = "A Stopwatch";
   btn4.textContent = "A Knife";
   }
-
+// change answers for 3rd question
   var changeq3= function changeanswer3 () {
     q.innerHTML= alltheQs.question3
     btn1.textContent = "Bear",
@@ -114,6 +123,7 @@ var changeq2= function changeanswer2() {
     btn3.textContent = "Wolf",
     btn4.textContent = "Snake"
   }
+// // change answers for 4th question
   var changeq4= function changeanswer4 () {
     q.innerHTML= alltheQs.question4
     btn1.textContent = "Mountain",
@@ -122,20 +132,16 @@ var changeq2= function changeanswer2() {
     btn4.textContent = "Sky"
   }
 
-var changeq5 = function changeanswer5 () {
-q.innerHTML= alltheQs.question5
-btn1.textContent = "The Night King's Army",
+// change answers for 5th question
+  var changeq5 = function changeanswer5 () {
+  q.innerHTML= alltheQs.question5
+  btn1.textContent = "The Night King's Army",
   btn2.textContent = "flock of doves",
   btn3.textContent = "teeth",
   btn4.textContent = "cows"
   }
 
-
-
 // working with button 1 
-// btn1.addEventListener("click", function () {
-//   changeq2();
-// });
 
 btn1.addEventListener("click", function (){
   console.log('button1 is the correct answer for question 1 and 4')
@@ -280,26 +286,7 @@ else if(currentQuestion==  alltheQs.question5){
 });
 
 
-
-
-// // when summit button get click, highscore card content appear, and input got insert in 
-// var recordpage= document.querySelector ("#scoreincard")
-// var submitbtn=document.querySelector ("#add-highScore")
-// function showRecord(){
-//  score.style.display= "none";
-//  recordpage.style.display="block";
+// function storeinitials() {
+//   // Stringify and set "todos" key in localStorage to todos array
+//   localStorage.setItem(, JSON.stringify(todos));
 // }
-// submitbtn.addEventListener("click", showRecord)
-
-
-// // collect input from the initial page 
-// 
-
-
-// var record= intInput.value.trim ()
-var intInputEl = document.querySelector("#initials")
-var record= intInputEl.value.trim ()
-function pleasetakeint (event){
-var record= intInputEl.value.trim ();
-console.log (record)
-}
